fb966c07cc746689be86c5d5388952fbe54d322b
Daniele Ricci <daniele@casaricci.it>
2020-05-07 13:16:32 +0200

    Simplify CA chain installation for Let's Encrypt
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- letsencrypt/post-hook.sh
+++ letsencrypt/post-hook.sh
@@ -15,13 +15,9 @@ install_certificate() {
         return
     fi
 
-    # download CA certificates
-    wget -q https://letsencrypt.org/certs/letsencryptauthorityx3.pem -O /tmp/level2.pem
-    wget -q https://letsencrypt.org/certs/isrgrootx1.pem -O /tmp/level1.pem
-
     cp ${LIVEDIR}/cert.pem ../config/certificate.pem
     cp ${LIVEDIR}/privkey.pem ../config/privatekey.pem
-    cat /tmp/level2.pem /tmp/level1.pem >../config/cachain.pem
+    cp ${LIVEDIR}/chain.pem ../config/cachain.pem
 }
 
 if [ -z "${XMPP_SERVICE}" ]; then
